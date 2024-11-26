f2f05a1e61cd81d631398e7f5e3f2278c150665f
jqxcz <jqxczz@gmail.com>
2024-11-26 19:11:26 +1100

    Packaging change -> bundle lib_cabaliser.so in wheel

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
-import distutils.command.build
 from setuptools import setup
+from setuptools.command.build_py import build_py
+import subprocess
 
-class BuildCommand(distutils.command.build.build):
-    def run(self):
-        distutils.command.build.build.run(self)
-        # Run makefile here
-           
+class BuildMake(build_py):
+    def run(self, *args, **kwargs):
+        subprocess.check_call(('make', 'lib_cabaliser.so'), cwd="c_lib")        
+        subprocess.check_call(('cp', 'c_lib/lib_cabaliser.so', 'src/cabaliser/lib_cabaliser.so'))
 
+        return super().run(*args, **kwargs)
 setup(
     name='cabaliser',
     packages=['cabaliser'],
-    package_dir={
-        '':'src'},
+    package_dir={'':'src'},
+    package_data={"cabaliser":["lib_cabaliser.so"]},
     include_package_data=True,
+    cmdclass={'build_py': BuildMake}
 )
