277b1168c226d32dbf63dcce27ea46c31d6e47ce
jqxcz <jqxczz@gmail.com>
2024-11-27 10:38:33 +1100

    Fix .so name overlap

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ import subprocess
 class BuildMake(build_py):
     def run(self, *args, **kwargs):
         subprocess.check_call(('make', 'lib_cabaliser.so'), cwd="c_lib")        
-        subprocess.check_call(('cp', 'c_lib/lib_cabaliser.so', 'src/cabaliser/lib_cabaliser.so'))
+        subprocess.check_call(('cp', 'c_lib/lib_cabaliser.so', 'src/cabaliser/lib_cabaliser_bin.so'))
 
         return super().run(*args, **kwargs)
 setup(
     name='cabaliser',
     packages=['cabaliser'],
     package_dir={'':'src'},
-    package_data={"cabaliser":["lib_cabaliser.so"]},
+    package_data={"cabaliser":["lib_cabaliser_bin.so"]},
     include_package_data=True,
     cmdclass={'build_py': BuildMake}
 )
--- src/cabaliser/lib_cabaliser.py
+++ src/cabaliser/lib_cabaliser.py
@@ -6,4 +6,4 @@ import os
 from ctypes import cdll
 
 lib_path = os.path.dirname(__file__)
-lib = cdll.LoadLibrary(f'{lib_path}/../../c_lib/lib_cabaliser.so')
+lib = cdll.LoadLibrary(f'{lib_path}/lib_cabaliser_bin.so')
