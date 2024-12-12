59ffae5a5152acb2ba83d26c4886c05afcfe5801
ahto <ahto@artifact8.xyz>
2024-12-12 19:14:08 +1100

    have to place identifier for sync, output on process

--- c_lib/benchmarks/scripts/sync_reports.sh
+++ c_lib/benchmarks/scripts/sync_reports.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
 
-if [ $# -ne 1 ]; then
-	echo "Missing target directory, please provide path that will sync with output/"
+if [ $# -ne 2 ]; then
+	echo "Missing either target directory and/or identifer"
+	echo "Please provide path that will sync with output/ and ident"
+
+	echo "Example: bash sync_reports.sh ~/reports qft_tests_wed"
 	exit 1
 fi
 
 
 TARGET_DIR=$1
+COMP_IDENT=$($2)
 SYNC_DATETIME=$(date  +"%Y%m%d_%H%M%S");
 SYNC_DATE=$(date +"%Y%m%d")
-COMP_HOST=$(whoami)
+OUTPATH="$TARGET_DIR/$SYNC_DATE-$COMP_IDENT-reports"
 
-rsync -r output/ $TARGET_DIR/$SYNC_DATE-$COMP_HOST-reports
+lscpu | head -n 18 | grep -v 'scaling' > $OUTPATH/cpu_info.txt 
 
+echo "Synchronising with folder"
+
+rsync -r output/$OUTPATH
 echo "Synchronised on $SYNC_DATETIME" > $TARGET_DIR/last_sync_date.txt
+
+echo "Completed"
