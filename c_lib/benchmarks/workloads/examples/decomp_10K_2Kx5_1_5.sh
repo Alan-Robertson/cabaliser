f8251a24e7b5c3f2a8382d68f794d11ef46f9fad
ahto <ahto@artifact8.xyz>
2024-12-13 02:18:22 +1100

    quick fix on argument that was set to 0 instead of 1

--- c_lib/benchmarks/workloads/examples/decomp_10K_2Kx5_1_5.sh
+++ c_lib/benchmarks/workloads/examples/decomp_10K_2Kx5_1_5.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-bash decomp_gen.sh 100000 20000 5 1 0 5 60 1 4 "decomp_benchmark_6K_Qbit_2Kx5Steps_1_Gates_5Steps_UpTo8Threads"
+bash decomp_gen.sh 100000 20000 5 1 1 1 60 1 4 "decomp_benchmark_6K_Qbit_2Kx5Steps_1_Gates_5Steps_UpTo8Threads"
